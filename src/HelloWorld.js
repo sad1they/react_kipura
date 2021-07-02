bd0405176d23971864734478a4315eaf67cc84c0
Sadi Zeynalov <51882998+sad1they@users.noreply.github.com>
2021-07-02 08:24:45 +0300

    Add files via upload

---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import "./HelloWorld.css";
+
+  class HelloWorld extends Component {
+    render() {
+      return (
+        <div className="helloContainer">
+          <h1>hello world!</h1>
+        </div>
+); }
+}
+  export default HelloWorld;
